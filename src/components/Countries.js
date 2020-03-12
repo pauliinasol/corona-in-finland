527eb50ecca34bd1304537a693bb93b782ae617d
Pauliina Solanne <pauliina.solanne@hoxhunt.com>
2020-03-12 19:26:06 +0200

    chart animation working

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,10 +1,18 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes, css } from "styled-components";
 import { HeadingTwo } from "../utils/typography";
 
 const CountryStyled = styled.div`
   list-style-type: none;
   text-align: left;
+`;
+
+const CountryCode = styled.div`
+  width: 70px;
+`;
+
+const CountriesLengthStyled = styled.div`
+  margin-left: 10px;
 `;
 
 const LineStyled = styled.div`
@@ -13,14 +21,23 @@ const LineStyled = styled.div`
   align-items: center;
   padding: 5px;
   > * {
-    margin: 10px;
+    /* margin: 5px; */
   }
 `;
 
 const Line = styled.div`
   background-color: #eb8a44;
   height: 10px;
-  width: 70px;
+  animation-iteration-count: 1;
+  animation-duration: 800ms;
+  animation-timing-function: ease-in;
+  animation-fill-mode: forwards;
+  ${({ fromY }) => css`
+    animation-name: ${keyframes`
+      0% { width: 0px }
+      100% { width: ${fromY * 3}px}
+    `};
+  `}
 `;
 
 export const CountryCard = ({ countries, countryOptions }) => {
@@ -35,7 +52,11 @@ export const CountryCard = ({ countries, countryOptions }) => {
           {countryOptions.map(c => (
             <li>
               <LineStyled>
-                {c} <Line /> {countriesLength(c)}
+                <CountryCode>{c}</CountryCode>
+                <Line fromY={countriesLength(c)} />
+                <CountriesLengthStyled>
+                  {countriesLength(c)}
+                </CountriesLengthStyled>
               </LineStyled>
             </li>
           ))}
